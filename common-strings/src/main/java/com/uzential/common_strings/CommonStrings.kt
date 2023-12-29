c29e9dbe5ffb696724bd2f206d46799b1acea083
Bogdan Pechounov <bogdan.pechounov@outlook.com>
2023-12-28 22:11:37 -0500

    ru, fr

--- common-strings/src/main/java/com/uzential/common_strings/CommonStrings.kt
+++ common-strings/src/main/java/com/uzential/common_strings/CommonStrings.kt
@@ -2,27 +2,32 @@ package com.uzential.common_strings
 
 object CommonStrings {
 
-    /**
-     * Settings screen
-     */
+
     val settings = CommonString(R.string._common_strings_settings)
+    val general = CommonString(R.string._common_strings_general)
+    val theme = CommonString(R.string._common_strings_theme)
+    val lightTheme = CommonString(R.string._common_strings_light_theme)
+    val darkTheme = CommonString(R.string._common_strings_dark_theme)
+    val device = CommonString(R.string._common_strings_device)
+
+    val menu = CommonString(R.string._common_strings_menu)
+    val try_again = CommonString(R.string._common_strings_try_again)
+    val import = CommonString(R.string._common_strings_import)
+    val go_back = CommonString(R.string._common_strings_go_back)
 
     //region Contact
-    val contact = CommonString(R.string._common_strings_contact)
-    /**
-     * Leave a review
-     */
-    val review = CommonString(R.string._common_strings_review)
 
-    /**
-     * Send feedback by email
-     */
+    val contact = CommonString(R.string._common_strings_contact)
+    val review = CommonString(R.string._common_strings_review)
     val feedback = CommonString(R.string._common_strings_feedback)
+
     // endregion
 
     // region Dialog
+
     val confirm = CommonString(R.string._common_strings_confirm)
     val cancel = CommonString(R.string._common_strings_cancel)
     val dismiss = CommonString(R.string._common_strings_dismiss)
+
     // endregion
 }
