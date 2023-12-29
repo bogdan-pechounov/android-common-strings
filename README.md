abfafafd9cff97ce3a944e62606589890bca56d3
Bogdan Pechounov <bogdan.pechounov@outlook.com>
2023-12-28 21:30:00 -0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# android-common-strings
+
+Some common strings used across projects.
+
+## References
+
+[The Missing Apple iOS Localization Term Glossary](https://www.ibabbleon.com/apple-ios-localization-term-glossary.html#request)
---
+++ common-strings/src/main/java/com/uzential/common_strings/CommonStrings.kt
@@ -0,0 +1,28 @@
+package com.uzential.common_strings
+
+object CommonStrings {
+
+    /**
+     * Settings screen
+     */
+    val settings = R.string._common_strings_settings
+
+    //region Contact
+    val contact = R.string._common_strings_contact
+    /**
+     * Leave a review
+     */
+    val review = R.string._common_strings_review
+
+    /**
+     * Send feedback by email
+     */
+    val feedback = R.string._common_strings_feedback
+    // endregion
+
+    // region Dialog
+    val confirm = R.string._common_strings_confirm
+    val cancel = R.string._common_strings_cancel
+    val dismiss = R.string._common_strings_dismiss
+    // endregion
+}
